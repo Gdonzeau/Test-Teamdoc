924694a485d0f77a7a086c5a028170c7e4c2ed54
GuillaumeDonzeau <gdonzeau@hotmail.com>
2022-06-03 18:06:04 +0200

    View des pays opérationnelle
    Ajouter aussi des images pré iOS14 pour les onglets

--- Test_TeamDoc/Test_TeamDoc/Controller/CountriesViewController.swift
+++ Test_TeamDoc/Test_TeamDoc/Controller/CountriesViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 class CountriesViewController: UIViewController {
     
     private let dataCountries = DataLoaded.allData.norm_Iso3166_2
-    private var countries = [String]()
+    private var countries = [Country]()
+    private var divisions = [String]()
+    private var firstStart = true
 
     @IBOutlet weak var countriesPickerView: UIPickerView!
     
@@ -21,16 +23,25 @@ class CountriesViewController: UIViewController {
         preparingDatas()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        divisionsTableView.reloadData()
+    }
+    
     func preparingDatas() {
+        // On charge un pays "vide" pour la présenation
+        let firstCountryEmpty = Country(name: "- -", divisions: [:])
+        countries.append(firstCountryEmpty)
         
         for dico in dataCountries {
             for (countryIndex, country) in dico {
                 print("Le pays \(country.name) a pour index \(countryIndex)")
-                countries.append(country.name)
+                countries.append(country)
             }
         }
     }
 }
+
+// MARK: - Gestion du PickerView
 
 extension CountriesViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -42,7 +53,33 @@ extension CountriesViewController: UIPickerViewDelegate, UIPickerViewDataSource 
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return countries[row]
+        return countries[row].name
     }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        divisions = [String]() // Réinitialisation
+        for (divisionIndex, division) in countries[row].divisions {
+            divisions.append("\(divisionIndex) \(division)")
+        }
+        divisionsTableView.reloadData()
+    }
+    
+}
+
+// MARK: - Gestion de la TableView
+
+extension CountriesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        divisions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DivisionCell", for: indexPath)
+        let division = divisions[indexPath.row]
+        cell.textLabel?.text = division
+        
+        return cell
+    }
+    
     
 }
